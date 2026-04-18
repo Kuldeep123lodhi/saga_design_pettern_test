7f90f8e0331f2a0a0c5bcd385133424ca6f3ff7c
kuldeep lodhi <kuldeeplodhi89@gmail.com>
2026-04-19 01:24:17 +0530

    first commit to test

--- payment-service/src/main/java/com/javatechie/PaymentEventsProducer.java
+++ payment-service/src/main/java/com/javatechie/PaymentEventsProducer.java
@@ -11,25 +11,25 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class PaymentEventsProducer {
 
-    private KafkaTemplate<String,Object> template;
+    private KafkaTemplate<String, Object> template;
 
     public PaymentEventsProducer(KafkaTemplate<String, Object> template) {
         this.template = template;
     }
 
-    public void publishPaymentSuccessEvent(SeatReservedEvent event){
+    public void publishPaymentSuccessEvent(SeatReservedEvent event) {
         log.info("Publishing payment success event ...");
-        BookingPaymentEvent paymentEvent=new BookingPaymentEvent(event.bookingId(),true,event.amount());
+        BookingPaymentEvent paymentEvent = new BookingPaymentEvent(event.bookingId(), true, event.amount());
         template
-                .send(KafkaConfigProperties.PAYMENT_EVENTS_TOPIC, event.bookingId(),paymentEvent);
+                .send(KafkaConfigProperties.PAYMENT_EVENTS_TOPIC, event.bookingId(), paymentEvent);
 
     }
 
-    public void publishPaymentFailureEvent(SeatReservedEvent event){
-        log.info("Publishing payment failure event ...");
-        BookingPaymentEvent paymentEvent=new BookingPaymentEvent(event.bookingId(),false,event.amount());
+    public void publishPaymentFailureEvent(SeatReservedEvent event) {
+        log.info("Publishing payment failure event ..." + "failling event due to payment");
+        BookingPaymentEvent paymentEvent = new BookingPaymentEvent(event.bookingId(), false, event.amount());
         template
-                .send(KafkaConfigProperties.PAYMENT_EVENTS_TOPIC, event.bookingId(),paymentEvent);
+                .send(KafkaConfigProperties.PAYMENT_EVENTS_TOPIC, event.bookingId(), paymentEvent);
 
     }
 }
